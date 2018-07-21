bc02ebf1a18e237c2a6f7a6a352eed4a44edc6b8
Muhammad <muhammadnagwere@gmail.com>
2018-07-22 00:29:18 +0300

    My first commit on this branch

--- app/src/main/java/com/example/eshop/DetailsActivity.java
+++ app/src/main/java/com/example/eshop/DetailsActivity.java
@@ -2,12 +2,48 @@ package com.example.eshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+TextView itemDisplay;
+TextView nameDisplay;
+TextView locationDisplay;
+TextView commentDisplay;
+TextView emailtDisplay;
+TextView modeOfDisplayDisplay;
+TextView phoneContactDisplay;
+TextView dateDisplay;
+TextView quantityDisplay;
+TextView amountDisplay;
+int itemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        nameDisplay=findViewById(R.id.nameDisplay);
+        phoneContactDisplay=findViewById(R.id.phoneContactDisplay);
+        emailtDisplay=findViewById(R.id.emailDisplay);
+        dateDisplay=findViewById(R.id.dateDisplay);
+        commentDisplay=findViewById(R.id.commentDisplay);
+        modeOfDisplayDisplay=findViewById(R.id.modeOfpaymentDisplay);
+        locationDisplay=findViewById(R.id.locationDisplay);
+        quantityDisplay=findViewById(R.id.quantityDisplay);
+        amountDisplay=findViewById(R.id.amountDisplay);
+        itemDisplay=findViewById(R.id.itemDisplay);
+
+    }
+    public void UpdateDetailsInterface(Order order){
+        nameDisplay.setText(order.getNameOfBuyer());
+        phoneContactDisplay.setText(order.getPhoneContact());
+        commentDisplay.setText(order.getComment());
+        modeOfDisplayDisplay.setText(order.getModeOfPayment());
+        locationDisplay.setText(order.getLocation());
+        itemDisplay.setText(order.getItemName());
+        amountDisplay.setText(order.getAmount());
+        quantityDisplay.setText(order.getQuantity());
+        dateDisplay.setText(order.getDate());
+
+
     }
 }
